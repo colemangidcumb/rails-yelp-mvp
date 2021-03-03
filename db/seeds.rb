f8efd3f0f7d02ec17c33f856240d79692bad978b
coleman gidcumb <colemangidcumb@gmail.com>
2021-03-03 08:23:00 -0700

    yelp has been created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+puts 'destroying restaurants'
+Restaurant.destroy_all
+5.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  puts "made #{restaurant.name} with an #{restaurant.id}"
+end
